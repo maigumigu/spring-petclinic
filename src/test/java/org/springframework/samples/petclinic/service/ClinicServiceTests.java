31a67765fa21b528bb9be7f4b0062c9c0008f6e1
migujeong@gmail.com <migujeong@gmail.com>
2019-12-19 09:27:44 +0900

    Change method name in test class

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
@@ -44,7 +44,7 @@ import org.springframework.transaction.annotation.Transactional;
  * TestContext Framework: </p> <ul> <li><strong>Spring IoC container caching</strong> which spares us unnecessary set up
  * time between test execution.</li> <li><strong>Dependency Injection</strong> of test fixture instances, meaning that
  * we don't need to perform application context lookups. See the use of {@link Autowired @Autowired} on the <code>{@link
- * ClinicServiceTests#clinicService clinicService}</code> instance variable, which uses autowiring <em>by
+ * ClinicServiceTests# clinicService}</code> instance variable, which uses autowiring <em>by
  * type</em>. <li><strong>Transaction management</strong>, meaning each test method is executed in its own transaction,
  * which is automatically rolled back by default. Thus, even if tests insert or otherwise change database state, there
  * is no need for a teardown or cleanup script. <li> An {@link org.springframework.context.ApplicationContext
@@ -74,10 +74,10 @@ class ClinicServiceTests {
 
     @Test
     void shouldFindOwnersByLastName() {
-        Collection<Owner> owners = this.owners.findByLastName("Davis");
+        Collection<Owner> owners = this.owners.findAllByLastNameContaining("Davis");
         assertThat(owners).hasSize(2);
 
-        owners = this.owners.findByLastName("Daviss");
+        owners = this.owners.findAllByLastNameContaining("Daviss");
         assertThat(owners).isEmpty();
     }
 
@@ -93,7 +93,7 @@ class ClinicServiceTests {
     @Test
     @Transactional
     void shouldInsertOwner() {
-        Collection<Owner> owners = this.owners.findByLastName("Schultz");
+        Collection<Owner> owners = this.owners.findAllByLastNameContaining("Schultz");
         int found = owners.size();
 
         Owner owner = new Owner();
@@ -105,7 +105,7 @@ class ClinicServiceTests {
         this.owners.save(owner);
         assertThat(owner.getId().longValue()).isNotEqualTo(0);
 
-        owners = this.owners.findByLastName("Schultz");
+        owners = this.owners.findAllByLastNameContaining("Schultz");
         assertThat(owners.size()).isEqualTo(found + 1);
     }
 
